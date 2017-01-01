d607532596e397fe354d61905dbd21b715115f1c
Joeun <imjoeunha@gmail.com>
2017-01-01 23:07:19 +0900

    modules: npm 초기화 및 기본 모듈 설치
    - react
    - react-dom
    - babel-preset-react
    - babel-preset-es2015

--- docs/javascript/index.js
+++
@@ -1,9 +0,0 @@
-navigator.geolocation.getCurrentPosition(
-  (position) => {
-    console.log(position);
-  },
-  (error) => {
-    console.error(error.message);
-  },
-  {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-)
