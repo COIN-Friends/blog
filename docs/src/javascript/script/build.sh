ae28f4d007d94f267425ee66ec265958e72a62d1
Joeun <imjoeunha@gmail.com>
2017-01-03 00:43:01 +0900

    blog: 블로그 구축을 위한 기본 설정 작업(1)

---
+++ docs/src/javascript/script/build.sh
@@ -0,0 +1,12 @@
+### JavsScript 트랜스파일
+babel --preset react,es2015 docs/src/javascript/source -d docs/src/javascript/build
+
+### JavaScript 패키징
+browserify docs/src/javascript/build/app.js -o docs/bundle.js
+
+### CSS 패키징
+cat docs/src/stylesheet/*/* docs/src/stylesheet/*.css | sed 's/..\/..\//src\//g'  > docs/bundle.css
+
+
+### 완료
+date; echo;
